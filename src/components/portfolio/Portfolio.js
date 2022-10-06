2564ef45f497d865a179719a6671340167944a41
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2022-10-06 14:02:18 +0200

    added screenshots

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,5 +1,41 @@
+import gamehub from "../../images/gamehub.png"
+import volleyblog from "../../images/volleyblog.png"
+import frameworks from "../../images/frameworks.png"
+
+
 export default function Portfolio() {
     return (
-        <h1>Portfolio</h1>
-    )
+        <>
+        <div>
+        <h1>My work and projects</h1>
+        <h2>Check out some of my projects throughout my studies.</h2>
+        <div className="projects-container">
+            <div>
+                <div>
+                    <img src={gamehub} alt="sneek peak of project"/>
+                </div>
+                <div>
+                    <a >Gamehub</a>
+                </div>
+            </div>
+            <div >
+            <div>
+                <img src={volleyblog} alt="sneek peak of project"/>
+            </div>
+            <div>
+                <a>Volleyball blog</a>
+            </div>
+            </div>
+            <div >
+            <div>
+                <img src={frameworks} alt="sneek peak of project"/>
+            </div>
+            <div>
+                <a>CSS Frameworks</a>
+            </div>
+            </div>
+        </div>
+    </div>
+    </>
+    );
   }
