261ffb52015248c10d355beb9fa68f64f0f5fe56
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2022-10-16 15:26:26 +0200

    l

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import meg from "../../images/meg-hjemme.png";
 import logo from "../../images/portfolio-logo-home.png";
 
@@ -10,7 +11,7 @@ export default function Home() {
             <img  src={logo} alt="sneek peak of project"/>
             <h1> My Portfolio</h1>
             <p> A student who are finished with my frontend development studies at Noroff. My focus is on writing clean and efficient code. I am a big fan of React, HTML5, CSS/SCSS and plain Javascript. </p>
-            <a className="cta-button" href="/portfolio"> Portfolio</a>
+            <Link className="cta-button" to="/portfolio"> Portfolio</Link> 
         </div>
         <div>
             <img className="meg-hjemme" src={meg} alt="sneek peak of project"/>
